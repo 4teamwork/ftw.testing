27c7c01fd7efc462123da11fd093541ac602593a
Jonas Baumann <jone@jone.ch>
2011-10-06 08:49:50 +0200

    Implement testcase.

---
+++ ftw/testing/__init__.py
@@ -0,0 +1,1 @@
+from ftw.testing.testcase import MockTestCase
---
+++ ftw/testing/testcase.py
@@ -0,0 +1,34 @@
+from plone import mocktestcase
+from zope.interface import alsoProvides
+from zope.interface import directlyProvides
+
+
+class MockTestCase(mocktestcase.MockTestCase):
+    """Advanced mock test case.
+    """
+
+    def providing_mock(self, interfaces, *args, **kwargs):
+        """Creates a new mock, based on a dummy object providing
+        `interfaces`. The first interface in `interfaces` is directly
+        provided, the rest are also-provided.
+        """
+
+        dummy = self.create_dummy()
+
+        first_interface = interfaces.pop(0)
+        directlyProvides(dummy, first_interface)
+
+        for iface in interfaces:
+            alsoProvides(dummy, iface)
+
+        kwargs['type'] = dummy
+
+        return self.mocker.mock(*args, **kwargs)
+
+    def stub(self, *args, **kwargs):
+        kwargs['count'] = False
+        return self.mocker.mock(*args, **kwargs)
+
+    def providing_stub(self, interfaces, *args, **kwargs):
+        kwargs['count'] = False
+        return self.providing_mock(interfaces, *args, **kwargs)
