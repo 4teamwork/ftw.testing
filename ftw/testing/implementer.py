68b2a2e43e50e84eb7bfc2b8ef8a975c4204ba41
Jonas Baumann <jone@jone.ch>
2012-03-13 22:32:48 +0100

    Add implementer, implementing an interface dynamically.

---
+++ ftw/testing/implementer.py
@@ -0,0 +1,49 @@
+from zope.interface import Attribute
+from zope.interface.interface import Method
+
+
+class Implementer(object):
+    """The implementer creates a class implementing an interface dinamically.
+    """
+
+    def __init__(self, interface):
+        self.interface = interface
+
+    def __call__(self):
+        impl = self._generate_class()
+        self._add_attributes(impl)
+        self._generate_methods(impl)
+        return impl
+
+    def _generate_class(self):
+        class Implementation(object):
+            pass
+
+        Implementation.__name__ = 'Implementation: %s' % self.interface.__name__
+        return Implementation
+
+    def _add_attributes(self, impl):
+        for name in self.interface.names():
+            obj = self.interface.get(name)
+            if isinstance(obj, Attribute):
+                setattr(impl, name, None)
+
+    def _generate_methods(self, impl):
+        for name in self.interface.names():
+            obj = self.interface.get(name)
+            if isinstance(obj, Method):
+                setattr(impl, name, self._generate_function(obj))
+
+    def _generate_function(self, interface_function):
+        signature = interface_function.getSignatureString()
+        if signature == '()':
+            signature = 'self'
+        else:
+            signature = 'self, %s' % signature[1:-1]
+
+        function_str = 'lambda %s: None' % signature
+        fun = eval(function_str)
+        fun.__name__ = interface_function.getName()
+        fun.__doc__ = interface_function.getDoc()
+
+        return fun
---
+++ ftw/testing/tests/test_implementer.py
@@ -0,0 +1,97 @@
+from ftw.testing.implementer import Implementer
+from unittest2 import TestCase
+from zope.interface import Interface, Attribute
+from zope.interface import implements
+import inspect
+
+
+class IExample(Interface):
+
+    foo = Attribute('An attribute.')
+
+    def no_arguments():
+        """A function without arguments.
+        """
+
+    def arguments(foo):
+        """A function with an argument.
+        """
+
+    def defaults(foo, bar=True):
+        """A function with defaults.
+        """
+
+    def magic_arguments(*args, **kwargs):
+        """A function with wildcard arguments and
+        keyword arguments.
+        """
+
+    def combined(foo, bar=True, *args, **kwargs):
+        """A function combining some signatures.
+        """
+
+
+class Example(object):
+    implements(IExample)
+
+    def no_arguments(self):
+        pass
+
+    def arguments(self, foo):
+        pass
+
+    def defaults(self, foo, bar=True):
+        pass
+
+    def magic_arguments(self, *args, **kwargs):
+        pass
+
+    def combined(self, foo, bar=True, *args, **kwargs):
+        pass
+
+
+class TestImplementer(TestCase):
+
+    def test_attributes(self):
+        generated = Implementer(IExample)()
+        self.assertTrue(hasattr(generated, 'foo'))
+
+    def test_no_arguments(self):
+        generated = Implementer(IExample)()
+        self.assertEquals(inspect.getargspec(generated.no_arguments),
+                          inspect.getargspec(Example.no_arguments))
+
+        self.assertEquals(IExample.get('no_arguments').__doc__,
+                          generated.no_arguments.__doc__)
+
+    def test_arguments(self):
+        generated = Implementer(IExample)()
+        self.assertEquals(inspect.getargspec(generated.arguments),
+                          inspect.getargspec(Example.arguments))
+
+        self.assertEquals(IExample.get('arguments').__doc__,
+                          generated.arguments.__doc__)
+
+    def test_defaults(self):
+        generated = Implementer(IExample)()
+        self.assertEquals(inspect.getargspec(generated.defaults),
+                          inspect.getargspec(Example.defaults))
+
+        self.assertEquals(IExample.get('defaults').__doc__,
+                          generated.defaults.__doc__)
+
+    def test_magic_arguments(self):
+        generated = Implementer(IExample)()
+        self.assertEquals(inspect.getargspec(generated.magic_arguments),
+                          inspect.getargspec(Example.magic_arguments))
+
+        self.assertEquals(IExample.get('magic_arguments').__doc__,
+                          generated.magic_arguments.__doc__)
+
+    def test_combined(self):
+        generated = Implementer(IExample)()
+        self.assertEquals(inspect.getargspec(generated.combined),
+                          inspect.getargspec(Example.combined))
+
+        self.assertEquals(IExample.get('combined').__doc__,
+                          generated.combined.__doc__)
