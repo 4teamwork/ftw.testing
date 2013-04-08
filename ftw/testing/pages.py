4776b5c306447631d3294bbc39429f28a1c3cb6d
Jonas Baumann <jone@jone.ch>
2013-04-08 10:08:54 +0200

    Fix error when filling fields and there is no field class.

--- ftw/testing/pages.py
+++ ftw/testing/pages.py
@@ -307,7 +307,8 @@ class FormPage(Plone):
             'Ambiguous matches for fields with labels "%s": %s' % (
             label, str(map(lambda item: item.outer_html, fields)))
 
-        if 'mce_editable' in fields.first['class'].split(' ') and \
+        if 'class' in fields.first and \
+                'mce_editable' in fields.first['class'].split(' ') and \
                 self.javascript_supported:
 
             # Typing in the iframe does not work with PhantomJS.
