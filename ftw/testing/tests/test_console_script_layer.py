49d7c68387434842a4bfd3136236b4cf95fc3ccb
Thomas Buchberger <t.buchberger@4teamwork.ch>
2019-04-17 16:41:39 +0200

    Use print function instead of statement

--- ftw/testing/tests/test_console_script_layer.py
+++ ftw/testing/tests/test_console_script_layer.py
@@ -26,6 +26,6 @@ class TestConsoleScriptLayer(TestCase):
         test_buildout_directory_is_isolated_for_each_test
 
     def test_python_script_with_environment_is_generated(self):
-        code = "from ftw.testing import layer; print layer.__name__"
+        code = "from ftw.testing import layer; print(layer.__name__)"
         exitcode, output = self.layer['execute_script']('py -c "{0}"'.format(code))
         self.assertEqual('ftw.testing.layer', output.splitlines()[0])
