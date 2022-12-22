f6cd42b04046d1a3c506108948942e532c789d35
Lukas Graf <lukas.graf@4teamwork.ch>
2022-12-22 12:48:53 +0100

    Fix genericsetup uninstall profile test:
    For Plone >= 5.2 a registry record 'custom_css_timestamp' for
    plone.app.theming.interfaces.IThemeSettings exists that screwed up the
    GS profile snapshot diffing.
    
    That record has both a datetime value and a defaultFactory with datetime.now()
    that need to be neutralized in order for the diff not to change all the time.

--- ftw/testing/__init__.py
+++ ftw/testing/__init__.py
@@ -5,7 +5,9 @@ from ftw.testing.layer import ComponentRegistryLayer
 from ftw.testing.staticuids import staticuid
 from ftw.testing.testcase import MockTestCase
 from ftw.testing.transaction_interceptor import TransactionInterceptor
+from Products.CMFPlone.utils import getFSVersionTuple
 import pkg_resources
 
 
 IS_PLONE_5 = pkg_resources.get_distribution('Products.CMFPlone').version >= '5'
+PLONE_VERSION = getFSVersionTuple()
